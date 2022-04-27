01f342cbc6c3872671914c435759a24c1a109659
rhams1997 <88038755+rhams1997@users.noreply.github.com>
2022-04-27 18:32:23 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,27 @@
 import Menu from '../components/Menu';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'reactstrap';
+import Head from 'next/head';
+
+import { Jumbotron, Container } from 'reactstrap';
 
 function Home() {
     return (
         <div>
 
-            <head>
+            <Head>
                 <title>Home-WallBotz</title>
                 <meta name='description' content='Site sobre robótica'/>
                 <meta name='autor' contet='Luiz Gustavo, Rafael Henrique e Yan Silveira'/>
-            </head>
+            </Head>
             <Menu />
 
             <Jumbotron fluid className="projetos">
                 <style>{`.projetos{
                         padding-top: 80px;
                         padding-bottom: 80px;
-                        background-color: blue;
-                        color: #00ff00;
+                        background-color: #DAA520;
+                        color: #fff;
                         margin-bottom: 0rem !important;
 
                 }`}</style>
@@ -32,25 +34,7 @@ function Home() {
                 </Container>
             </Jumbotron>
 
-            <Jumbotron fluid className="projetos">
-                <style>{`.projetos{
-                        padding-top: 80px;
-                        padding-bottom: 80px;
-                        background-color: #00ff00;
-                        color: #00ff00;
-                        
 
-                }`}</style>
-
-                <Container className='text-center'>
-                    
-                    <h1 className='display-4'>Já ouviu falar em robótica no IF?</h1>
-                    <p className='lead'>A robótica é um projeto de extenção do IF Salinas, que foi fundado em 2018.</p>
-
-                </Container>
-            </Jumbotron>
-
-            
 
         </div>
     );

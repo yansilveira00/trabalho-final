a27334b3ecd9d244060966541428ed731bccb3fb
Phelps671 <74880596+Phelps671@users.noreply.github.com>
2022-05-01 23:26:56 -0300

    teste1.0

--- pages/eventos.js
+++ pages/eventos.js
@@ -1,43 +1,125 @@
 import Menu from '../components/Menu';
-
+import React from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Head from 'next/head';
 
-import {Jumbotron, Container} from 'reactstrap';
+import { Jumbotron, Container } from 'reactstrap';
 
-function Eventos() {
-    return (
+const Home = (data) => (
         <div>
+
+            <Head>
+                <title>Home-WallBotz</title>
+                <meta name='description' content='Site sobre robótica'/>
+                <meta name='autor' contet='Luiz Gustavo, Rafael Henrique e Yan Silveira'/>
+            </Head>
             <Menu />
-            
-            <Jumbotron>
-            <style>{`.sobre{
+
+            <Jumbotron fluid className="projetos">
+                <style>{`.projetos{
                         padding-top: 80px;
                         padding-bottom: 80px;
                         background-color: #DAA520;
                         color: #fff;
                         margin-bottom: 0rem !important;
+
+                }
+                .circulo{
+                    width: 140px;
+                    height: 140px;
+                    background-color: blue;
+                    font-size: 52px;
+                    padding-top: 24px;
+
+                }
+
+                .ang, .penha, .welisom{
+                    width= 100px;
+                    height= 100px;
+                    font-size= 40px; 
+                    padding-top= 20px; 
+                }
+                .centralizar{
+                    margin: 0 auto !important;
+                    float: none !important;
+                }
+                .featurette-divider {
+                    margin: 5rem 0; 
+                  }
+
                 }`}</style>
-                <Container >
-                    <div className='text-center'>
-                        <h1 className='display-4'>Eventos da Robótica!</h1>
 
-                    </div>
-                    
-                </Container>
-            </Jumbotron>
 
-            <Jumbotron>
-                <Container>
+                <Container className='text-center'>
                     <div>
-                        
+                        {console.log(data)}
+                        <h1 className='display-4'>{data.response.titulo}</h1>
+                        <p className='lead'>{data.response.subtitulo}</p>
                     </div>
+
+                    <hr class="featurette-divider"/>
+
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <div >
+                                <img className="ang " src="ang_joca.png" />
+                            </div>
+                            
+                            <h2 className='mt-4 mt-4'>{data.response.servUmTitulo}</h2>
+                            <p>{data.response.servUmDescricao}</p>
+                            
+                        </div>
+                        <div className="col-lg-4">
+                            <div >
+                                <img className="welisom" src="welisom_joca.png" />
+                            </div>
+
+                            <h2 className='mt-4 mt-4'>{data.response.servDoisTitulo}</h2>
+                            <p>{data.response.servDoisDescricao}</p>
+                            
+                        </div>
+                        <div className="col-lg-4">
+                            <div >
+                                <img className="penha" src="penha_joca.png" />
+
+                            </div>
+
+                            <h2 className='mt-4 mt-4'>{data.response.servTresTitulo}</h2>
+                            <p>{data.response.servTresDescricao}</p>
+                    
+                        </div>
+                        
+
+                        </div>
+
+                        <hr class="featurette-divider"/>
+
+                        <div  className="row featurette">
+                        <div className="col-md-7">
+                            <h2 className="featurette-heading">{data.response.servQuatroTitulo} <span class="text-muted">{data.response.servQuatroSubtitulo}</span></h2>
+                            <p className="lead">{data.response.servQuatroDescricao}</p>
+                        </div>
+                        <div className="col-md-5">
+                            <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src="equipeRobo.jpg" width="500" height="500" focusable="false"></img>
+                        </div>
+                        </div>
+
+                                  
+
                 </Container>
+               
             </Jumbotron>
 
-
-            
         </div>
     );
-}
 
-export default Eventos
+    Home.getInitialProps = async () => {
+       var response = await axios.get('http://localhost:8010/home');
+
+       console.log(response.data);
+
+       return {response: response.data}
+    }
+
+export default Home;
--- pages/quemsomos.js
+++ pages/quemsomos.js
@@ -1,69 +1,125 @@
 import Menu from '../components/Menu';
-
+import React from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Head from 'next/head';
 
-import {Jumbotron, Container} from 'reactstrap';
+import { Jumbotron, Container } from 'reactstrap';
 
-function QuemSomos() {
-    return (
+const Home = (data) => (
         <div>
+
+            <Head>
+                <title>Home-WallBotz</title>
+                <meta name='description' content='Site sobre robótica'/>
+                <meta name='autor' contet='Luiz Gustavo, Rafael Henrique e Yan Silveira'/>
+            </Head>
             <Menu />
-            <Jumbotron fluid className="sobre">
-            <style>{`.sobre{
+
+            <Jumbotron fluid className="projetos">
+                <style>{`.projetos{
                         padding-top: 80px;
                         padding-bottom: 80px;
                         background-color: #DAA520;
                         color: #fff;
                         margin-bottom: 0rem !important;
+
+                }
+                .circulo{
+                    width: 140px;
+                    height: 140px;
+                    background-color: blue;
+                    font-size: 52px;
+                    padding-top: 24px;
+
+                }
+
+                .ang, .penha, .welisom{
+                    width= 100px;
+                    height= 100px;
+                    font-size= 40px; 
+                    padding-top= 20px; 
+                }
+                .centralizar{
+                    margin: 0 auto !important;
+                    float: none !important;
+                }
+                .featurette-divider {
+                    margin: 5rem 0; 
+                  }
+
                 }`}</style>
 
-                <Container>
-                    <div className="text-center">
-                        <h1 className='display-4'>Sobre a Robótica</h1>
+
+                <Container className='text-center'>
+                    <div>
+                        {console.log(data)}
+                        <h1 className='display-4'>{data.response.titulo}</h1>
+                        <p className='lead'>{data.response.subtitulo}</p>
                     </div>
 
+                    <hr class="featurette-divider"/>
 
-                </Container>
-            </Jumbotron>
-
-            <Jumbotron fluid className="head-sobre">
-            <style>{`.head-sobre{
-                        padding-top: 80px;
-                        padding-bottom: 80px;
-                        background-color: #DAA520;
-                        color: #fff;
-                        margin-bottom: 0rem !important;
-                    }
-                    .featurette-divider {
-                        margin: 5rem 0; 
-                      }
-                }`}</style>
-
-                <Container>
-                       
-                        <div class="col-md-8">
-                        <h3 class="pb-4 mb-4 fst-italic border-bottom">
-                            O Começo de Tudo
-                        </h3>
-                        
-                        <article>
-                            <p>Os alunos do Instituto Federal do Norte de Minas Gerais, Ângella e Wellison, munidos pela vontade de criar um projeto de extensão voltado para a robótica, buscaram um(a) coordenador(a) para o mesmo, encontrando a Docente Maria da Penha, juntos criaram o projeto.</p>
-                            <hr/>
-                            <p>Ângella era da Licenciatura em Física, Wellison era do Curso Tecnico em Informática e Maria da Penha, Docente da Area de Física</p>
-                            <h2>Objetivo</h2>
-                            <p>Com objetivo de instigar os alunos ao estudo de conceitos físicos, os presentes acadêmicos do curso de licenciatura do IFNMG- Campus Salinas, desenvolveram robôs visando propiciar um ambiente estimulante e favorável ao ensino de física. O projeto foi coordenado pela professora de física, Maria da Penha, em 2020.</p>
-                            <h3>Aplicação</h3>
-                            <p>Visando utilizar ferramentas que aguçam a investigação científica, fomentou o desenvolvimento de robôs para serem utilizados de maneira prática no ensino/aprendizagem de física. Os aparatos são utilizados para demonstrar os conceitos de maneira aplicável, e para isso utilizam-se todas as etapas da montagem dos robôs e também seu funcionamento. No início dos trabalhos, os discentes elaboram projetos, visando utilizá-los para ensinar um tópico de física no ensino médio.</p>
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <div >
+                                <img className="ang " src="ang_joca.png" />
+                            </div>
                             
-                        </article>
+                            <h2 className='mt-4 mt-4'>{data.response.servUmTitulo}</h2>
+                            <p>{data.response.servUmDescricao}</p>
+                            
+                        </div>
+                        <div className="col-lg-4">
+                            <div >
+                                <img className="welisom" src="welisom_joca.png" />
+                            </div>
 
-                        </div>   
+                            <h2 className='mt-4 mt-4'>{data.response.servDoisTitulo}</h2>
+                            <p>{data.response.servDoisDescricao}</p>
+                            
+                        </div>
+                        <div className="col-lg-4">
+                            <div >
+                                <img className="penha" src="penha_joca.png" />
+
+                            </div>
+
+                            <h2 className='mt-4 mt-4'>{data.response.servTresTitulo}</h2>
+                            <p>{data.response.servTresDescricao}</p>
+                    
+                        </div>
+                        
+
+                        </div>
+
+                        <hr class="featurette-divider"/>
+
+                        <div  className="row featurette">
+                        <div className="col-md-7">
+                            <h2 className="featurette-heading">{data.response.servQuatroTitulo} <span class="text-muted">{data.response.servQuatroSubtitulo}</span></h2>
+                            <p className="lead">{data.response.servQuatroDescricao}</p>
+                        </div>
+                        <div className="col-md-5">
+                            <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src="equipeRobo.jpg" width="500" height="500" focusable="false"></img>
+                        </div>
+                        </div>
+
+                                  
 
                 </Container>
+               
             </Jumbotron>
-            
+
         </div>
     );
-}
 
-export default QuemSomos
+    Home.getInitialProps = async () => {
+       var response = await axios.get('http://localhost:8010/home');
+
+       console.log(response.data);
+
+       return {response: response.data}
+    }
+
+export default Home;

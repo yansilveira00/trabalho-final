598ef1c6c99666f699f0f4bc1d7142e52ac2998f
Phelps671 <74880596+Phelps671@users.noreply.github.com>
2022-05-02 22:37:34 -0300

    Teste.Insomia2.0

--- pages/projetos.js
+++ pages/projetos.js
@@ -1,116 +1,125 @@
-import React from 'react';
 import Menu from '../components/Menu';
-import { UncontrolledCarousel } from 'reactstrap';
-
+import React from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Head from 'next/head';
 
-import {Jumbotron, Container} from 'reactstrap';
+import { Jumbotron, Container } from 'reactstrap';
 
-function Projetos() {
-    return (
+const Home = (data) => (
         <div>
+
+            <Head>
+                <title>Home-WallBotz</title>
+                <meta name='description' content='Site sobre robótica'/>
+                <meta name='autor' contet='Luiz Gustavo, Rafael Henrique e Yan Silveira'/>
+            </Head>
             <Menu />
 
-            <Jumbotron fluid className='sobre'>
-            <style>{`.sobre{
+            <Jumbotron fluid className="projetos">
+                <style>{`.projetos{
                         padding-top: 80px;
                         padding-bottom: 80px;
                         background-color: #DAA520;
                         color: #fff;
                         margin-bottom: 0rem !important;
+
+                }
+                .circulo{
+                    width: 140px;
+                    height: 140px;
+                    background-color: blue;
+                    font-size: 52px;
+                    padding-top: 24px;
+
+                }
+
+                .ang, .penha, .welisom{
+                    width= 100px;
+                    height= 100px;
+                    font-size= 40px; 
+                    padding-top= 20px; 
+                }
+                .centralizar{
+                    margin: 0 auto !important;
+                    float: none !important;
+                }
+                .featurette-divider {
+                    margin: 5rem 0; 
+                  }
+
                 }`}</style>
-                <Container >
-                    <div className='text-center'>
-                    <h1 className='display-4'>Projetos</h1>
 
+
+                <Container className='text-center'>
+                    <div>
+                        {console.log(data)}
+                        <h1 className='display-4'>{data.response.titulo}</h1>
+                        <p className='lead'>{data.response.subtitulo}</p>
                     </div>
+
+                    <hr class="featurette-divider"/>
+
+                    <div className="row">
+                        <div className="col-lg-4">
+                            <div >
+                                <img className="ang " src="ang_joca.png" />
+                            </div>
+                            
+                            <h2 className='mt-4 mt-4'>{data.response.servUmTitulo}</h2>
+                            <p>{data.response.servUmDescricao}</p>
+                            
+                        </div>
+                        <div className="col-lg-4">
+                            <div >
+                                <img className="welisom" src="welisom_joca.png" />
+                            </div>
+
+                            <h2 className='mt-4 mt-4'>{data.response.servDoisTitulo}</h2>
+                            <p>{data.response.servDoisDescricao}</p>
+                            
+                        </div>
+                        <div className="col-lg-4">
+                            <div >
+                                <img className="penha" src="penha_joca.png" />
+
+                            </div>
+
+                            <h2 className='mt-4 mt-4'>{data.response.servTresTitulo}</h2>
+                            <p>{data.response.servTresDescricao}</p>
                     
+                        </div>
+                        
+
+                        </div>
+
+                        <hr class="featurette-divider"/>
+
+                        <div  className="row featurette">
+                        <div className="col-md-7">
+                            <h2 className="featurette-heading">{data.response.servQuatroTitulo} <span class="text-muted">{data.response.servQuatroSubtitulo}</span></h2>
+                            <p className="lead">{data.response.servQuatroDescricao}</p>
+                        </div>
+                        <div className="col-md-5">
+                            <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src="equipeRobo.jpg" width="500" height="500" focusable="false"></img>
+                        </div>
+                        </div>
+
+                                  
+
                 </Container>
+               
             </Jumbotron>
 
-            <Jumbotron fluid className='sobre'>
-            
-                <Container>
-                    <div class="row featurette">
-                      <div class="col-md-7">
-                        <h2 class="featurette-heading"> Carrinho de controle remoto feito de palitos de picolé. <span class="text-muted"></span></h2>
-                        <p class="lead"></p>
-                      </div>
-                      <div class="col-md-5">
-                      <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src="carrinho.jpeg" width="500" height="500" focusable="false"></img>
-
-                      </div>
-                    </div>
-
-                    <hr class="featurette-divider"/>
-
-                    <div class="row featurette">
-                      <div class="col-md-7 order-md-2">
-                        <h2 class="featurette-heading"> Braço Robotico <span class="text-muted"></span></h2>
-                        <p class="lead"></p>
-                      </div>
-                      <div class="col-md-5 order-md-1">
-                      <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src="robotico.jpg" width="500" height="500" focusable="false"></img>
-
-                      </div>
-                    </div>
-
-                    <hr class="featurette-divider"/>
-
-                    <div class="row featurette">
-                      <div class="col-md-7">
-                        <h2 class="featurette-heading">Robô Explorador <span class="text-muted"></span></h2>
-                        <p class="lead"></p>
-                      </div>
-                      <div class="col-md-5">
-                      <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src="esplorador.jpg" width="500" height="500" focusable="false"></img>
-
-                      </div>
-                    </div>
-
-                    <hr class="featurette-divider"/>
-
-
-                    <div class="row featurette">
-                      <div class="col-md-7">
-                        <h2 class="featurette-heading"> Robô X1 <span class="text-muted"></span></h2>
-                        <p class="lead"></p>
-                      </div>
-                      <div class="col-md-5">
-                      <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src="x1.jpeg" width="500" height="500" focusable="false"></img>
-
-                      </div>
-                    </div>
-
-                    <hr class="featurette-divider"/>
-
-                    <div class="row featurette">
-                      <div class="col-md-7 order-md-2">
-                        <h2 class="featurette-heading"> Seguidor de Linhas <span class="text-muted"></span></h2>
-                        <p class="lead"></p>
-                      </div>
-                      <div class="col-md-5 order-md-1">
-                      <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src="seguidor.jpg" width="500" height="500" focusable="false"></img>
-
-                      </div>
-                    </div>
-
-                    <hr class="featurette-divider"/>
-
-                    <div class="row featurette">
-                      <div class="col-md-7">
-                        <h2 class="featurette-heading">Drone <span class="text-muted"></span></h2>
-                        <p class="lead"></p>
-                      </div>
-                      <div class="col-md-5">
-                      <img className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src="drone1.jpeg" width="500" height="500" focusable="false"></img>
-
-                      </div>
-                    </div>
-                </Container>
-            </Jumbotron>
         </div>
     );
-}
 
-export default Projetos
+    Home.getInitialProps = async () => {
+       var response = await axios.get('http://localhost:8010/home');
+
+       console.log(response.data);
+
+       return {response: response.data}
+    }
+
+export default Home;
